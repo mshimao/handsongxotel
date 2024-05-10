4feb56c27343ae8359808f7994cf192935b35ea6
Mario K. Shimao <mario.shimao@aglsolutions.com.br>
2024-05-10 10:28:55 -0300

    otel ok

--- CSharpSample/entrypoint.sh
+++ CSharpSample/entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
-.otel-dotnet-auto/instrument.sh
+# .otel-dotnet-auto/instrument.sh
+export OTEL_TRACES_EXPORTER=none \
+  OTEL_METRICS_EXPORTER=none \
+  OTEL_LOGS_EXPORTER=none \
+  OTEL_DOTNET_AUTO_TRACES_CONSOLE_EXPORTER_ENABLED=true \
+  OTEL_DOTNET_AUTO_METRICS_CONSOLE_EXPORTER_ENABLED=true \
+  OTEL_DOTNET_AUTO_LOGS_CONSOLE_EXPORTER_ENABLED=true \
+  OTEL_SERVICE_NAME=csharpsample
+. $HOME/.otel-dotnet-auto/instrument.sh
 dotnet CSharpSample.dll
 
